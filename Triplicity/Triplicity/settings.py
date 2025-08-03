a746afd0e5a17bf0a7c3aee46a50a5f6d962836f
ANISH-2814 <anishbawdhankar@users.noreply.github.com>
2025-08-04 00:54:36 +0530

    final cleanup before deploying

--- Triplicity/Triplicity/settings.py
+++ Triplicity/Triplicity/settings.py
@@ -15,10 +15,14 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-##r1vx9$^*@^1ws3_57mq
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 #DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [    
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com'
+    ]
 
 # Application definition
 
@@ -94,11 +98,14 @@ AUTH_USER_MODEL = 'accounts.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='triplicity_db'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default='Anish@postgres'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 # Password validation
@@ -159,9 +166,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='test.triplicity@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='khqtbdqldudlilez')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='test.triplicity@gmail.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 # Login/Logout URLs
 LOGIN_URL = '/accounts/login/'
